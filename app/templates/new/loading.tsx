b77d9ac94ef195fafce343e277e667d9aaf1d481
AverageStudioss <matyaskrupicka@icloud.com>
2026-06-22 11:48:54 -0400

    speed

--- app/templates/new/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { LaunchWizardLoading } from "@/components/app-route-loading";
-
-export default function Loading() {
-  return <LaunchWizardLoading />;
-}
--- app/workspace/settings/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { AppRouteLoading } from "@/components/app-route-loading";
-
-export default function Loading() {
-  return <AppRouteLoading label="Loading settings…" />;
-}
